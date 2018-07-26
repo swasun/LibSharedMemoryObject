08a0ddf9bfa131314c422a3d9d944d00f668c453
Charly Lamothe <charly.lamothe@sfr.fr>
2018-07-26 10:58:08 +0200

    Update licence and copyright from gpl3 to apache2

--- src/smo/impl/windows/MemoryModule.c
+++ src/smo/impl/windows/MemoryModule.c
@@ -1,34 +1,20 @@
-/*
- * Memory DLL loading code
- * Version 0.0.4
- *
- * Copyright (c) 2004-2015 by Joachim Bauch / mail@joachim-bauch.de
- * http://www.joachim-bauch.de
- *
- * The contents of this file are subject to the Mozilla Public License Version
- * 2.0 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- * for the specific language governing rights and limitations under the
- * License.
- *
- * The Original Code is MemoryModule.c
- *
- * The Initial Developer of the Original Code is Joachim Bauch.
- *
- * Portions created by Joachim Bauch are Copyright (C) 2004-2015
- * Joachim Bauch. All Rights Reserved.
- *
- *
- * THeller: Added binary search in MemoryGetProcAddress function
- * (#define USE_BINARY_SEARCH to enable it).  This gives a very large
- * speedup for libraries that exports lots of functions.
- *
- * These portions are Copyright (C) 2013 Thomas Heller.
- */
+/*******************************************************************************
+ * Copyright (C) 2018 Charly Lamothe                                           *
+ *                                                                             *
+ * This file is part of LibSharedMemoryObject.                                 *
+ *                                                                             *
+ *   Licensed under the Apache License, Version 2.0 (the "License");           *
+ *   you may not use this file except in compliance with the License.          *
+ *   You may obtain a copy of the License at                                   *
+ *                                                                             *
+ *   http://www.apache.org/licenses/LICENSE-2.0                                *
+ *                                                                             *
+ *   Unless required by applicable law or agreed to in writing, software       *
+ *   distributed under the License is distributed on an "AS IS" BASIS,         *
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  *
+ *   See the License for the specific language governing permissions and       *
+ *   limitations under the License.                                            *
+ *******************************************************************************/
 
 #include <windows.h>
 #include <winnt.h>
